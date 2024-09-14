8fd370906ed3df12c294c20eb966077bdfd79009
Shubham Bhardwaj <shubham998845@gmail.com>
2024-09-14 17:43:41 +0530

    initialwork

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import { HomeArt } from "@/components/HomeArt/HomeArt";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Home</h1>
+      <HomeArt />
     </div>
   );
 }
---
+++ src/components/HomeArt/HomeArt.jsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "./HomeArt.module.scss";
+import Image from "next/image";
+
+export const HomeArt = () => {
+  return (
+    <div className={styles.container}>
+      <motion.div
+        className={styles.imageContainer}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <Image
+          width={1200}
+          height={1200}
+          src="/images/shubh5.png"
+          alt="Shubham Bhardwaj"
+          className={styles.photo}
+        />
+      </motion.div>
+      <motion.div
+        className={styles.textContainer}
+        initial={{ opacity: 0, x: -50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <h1 className={styles.name}>Shubh</h1>
+        {/* <p className={styles.title}>Frontend Developer</p>
+        <p className={styles.description}>
+          Crafting elegant and efficient user interfaces with a focus on modern
+          web technologies.
+        </p> */}
+      </motion.div>
+    </div>
+  );
+};
