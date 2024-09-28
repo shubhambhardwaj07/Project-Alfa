ebf2e30cc54adeeda26e37dc108cbd308e09ffca
Shubham Bhardwaj <shubham998845@gmail.com>
2024-09-28 13:58:09 +0530

    locomotive

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,10 @@
+import { PunjabMap } from "@/components/PunjabMap/PunjabMap";
+
 export default function About() {
   return (
     <div>
       <h1>About</h1>
+      <PunjabMap />
     </div>
   );
 }
