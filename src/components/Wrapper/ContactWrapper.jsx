3df502f49a5fa0b2bb1a4db09eae9f0f0a657c29
Shubham Bhardwaj <shubham998845@gmail.com>
2024-09-30 12:22:32 +0530

    window not defined fixed

--- src/components/Wrapper/ContactWrapper.jsx
+++ src/components/Wrapper/ContactWrapper.jsx
@@ -3,9 +3,12 @@
 import dynamic from "next/dynamic";
 import { ContactForm } from "../ContactForm/ContactForm";
 
-const PunjabMap = dynamic(() => import("../PunjabMap/PunjabMap"), {
-  ssr: false,
-});
+const PunjabMap = dynamic(
+  () => import("../PunjabMap/PunjabMap").then((module) => module.PunjabMap),
+  {
+    ssr: false,
+  }
+);
 
 export const ContactWrapper = () => {
   return (
